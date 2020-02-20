f4189c77ae19ebb3ba4a46ba3e0e7d12fb29a80f
Valentin Quintero <valentin.quintero@ceiba.com.co>
2020-02-20 15:11:41 -0500

    Multi-module project, all the source files from parent project have been removed

---
+++ pet-clinic-data/src/test/java/example/springframework/petclinic/FirstTest.java
@@ -0,0 +1,17 @@
+package example.springframework.petclinic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FirstTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @Test
+    void test() {
+        
+    }
+
+}
