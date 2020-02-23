758247fadb704aeba2e47dc4c3777d7497e70547
Valentin Quintero <valentin.quintero@ceiba.com.co>
2020-02-23 11:36:48 -0500

    List vets using map vet service. Closes #15

--- pet-clinic-web/src/main/java/example/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/example/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package example.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import example.springframework.petclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
+    
+    private final VetService vetService;
+    
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String getVetList() {
+    public String getVetList(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
     
