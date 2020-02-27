5bd012382bf05674e83b191e940133773084dcf4
Valentin Quintero <valentin.quintero@ceiba.com.co>
2020-02-27 11:44:20 -0500

    Create Specialty map service impl. Closes #29

---
+++ pet-clinic-data/src/main/java/example/springframework/petclinic/map/SpecialtyServiceMap.java
@@ -0,0 +1,8 @@
+package example.springframework.petclinic.map;
+
+import example.springframework.petclinic.model.Specialty;
+import example.springframework.petclinic.services.SpecialtyService;
+
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+
+}
---
+++ pet-clinic-data/src/main/java/example/springframework/petclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package example.springframework.petclinic.services;
+
+import example.springframework.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long>{
+
+}
