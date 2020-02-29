984172a1b0cca6866a19920bf8fa5d854d607b8f
Valentin Quintero <valentin.quintero@ceiba.com.co>
2020-02-29 12:59:27 -0500

    Added missing service annotation to specialty jpa service

--- pet-clinic-data/src/main/java/example/springframework/petclinic/services/springdatajpa/SpecialtySpringDataJpaService.java
+++ pet-clinic-data/src/main/java/example/springframework/petclinic/services/springdatajpa/SpecialtySpringDataJpaService.java
@@ -1,9 +1,14 @@
 package example.springframework.petclinic.services.springdatajpa;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
 import example.springframework.petclinic.model.Specialty;
 import example.springframework.petclinic.repositories.SpecialtyRepository;
 import example.springframework.petclinic.services.SpecialtyService;
 
+@Service
+@Profile("springdatajpa")
 public class SpecialtySpringDataJpaService extends AbstractSpringDataJpaService<Specialty, Long, SpecialtyRepository> implements SpecialtyService {
 
     public SpecialtySpringDataJpaService(SpecialtyRepository specialtyRepository) {
