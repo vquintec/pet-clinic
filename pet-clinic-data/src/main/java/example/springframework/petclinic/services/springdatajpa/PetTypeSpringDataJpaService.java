5680db7bef3d3a5bf1aca3a77daa23800c0d5736
Valentin Quintero <valentin.quintero@ceiba.com.co>
2020-02-29 12:43:38 -0500

    Create Spring Data JPA service for pet type. Closes #40

---
+++ pet-clinic-data/src/main/java/example/springframework/petclinic/services/springdatajpa/PetTypeSpringDataJpaService.java
@@ -0,0 +1,18 @@
+package example.springframework.petclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import example.springframework.petclinic.model.PetType;
+import example.springframework.petclinic.repositories.PetTypeRepository;
+import example.springframework.petclinic.services.PetTypeService;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSpringDataJpaService extends AbstractSpringDataJpaService<PetType, Long, PetTypeRepository> implements PetTypeService {
+
+    public PetTypeSpringDataJpaService(PetTypeRepository objectRepository) {
+        super(objectRepository);
+    }
+
+}
